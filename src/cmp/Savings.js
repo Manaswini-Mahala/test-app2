52358dbfd17b53d42bb9bc84bcacb3b0ea5e9ea0
Manaswini-Mahala <mahalamanaswini@gmail.com>
2022-03-28 19:41:40 +0530

    Making chnages to savings.js

--- src/cmp/Savings.js
+++ src/cmp/Savings.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
-class Savings extends Component{
-    render(){
-        return(
+class Savings extends Component {
+    render() {
+        return (
             <div className='app'>
-                Savings Component
-                
+                <label>Account Balance</label><br />
+                <input type="text" id='cbalance'
+                    onChange={(e) => { this.setState({ cbalance: e.target.value }) }} /><br /><br />
+
+                <label>Month</label><br />
+                <input type="text" id='month'
+                    onChange={(e) => { this.setState({ month: e.target.value }) }} /><br /><br />
+
             </div>
         );
     }
